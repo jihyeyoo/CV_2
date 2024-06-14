76441a4480409f8c5a1ef6517b29c58133a838c5
pearlish <jinjoo2257@gmail.com>
2024-06-14 23:32:58 +0200

    Update problem2.py

--- A2/problem2.py
+++ A2/problem2.py
@@ -4,7 +4,7 @@ from scipy import interpolate   # Use this for interpolation
 from scipy import signal        # Feel free to use convolutions, if needed
 from scipy import optimize      # For gradient-based optimisation
 from PIL import Image           # For loading images
-
+import matplotlib.pyplot as plt
 # for experiments with different initialisation
 from problem1 import random_disparity
 from problem1 import constant_disparity
@@ -107,33 +107,11 @@ def shift_interpolated_disparity(im1, d):
         im1_shifted: Shifted version of im1 by the disparity value.
     """
 
-    def bilinear_interpolate(y, x, img):
-        left, top = int(x), int(y)
-        right, bottom = min(left + 1, img.shape[1]-1), min(top + 1, img.shape[0]-1)
-
-        x_lambda, y_lambda = x - left, y - top
-
-        top_x_intp = (1 - x_lambda) * img[top][left] + x_lambda * img[top][right]
-        bottom_x_intp = (1 - x_lambda) * img[bottom][left] + x_lambda * img[bottom][right]
-
-        return (1 - y_lambda) * top_x_intp + y_lambda * bottom_x_intp
-
-
-    shifted_im1 = np.zeros(im1.shape)
-    for i in range(im1.shape[0]):
-        for j in range(im1.shape[1]):
-            new_j = j - d[i,j]
-            if new_j < im1.shape[1] - 1 and new_j >= 0:
-                shifted_im1[i][j] = bilinear_interpolate(i, new_j, im1)
-
-            left = int(new_j)
-            top = i
-
-            # shifted_im1[i][j] = interpolate.RegularGridInterpolator(
-            #     points=[(top, left), (top, left + 1), (top+1, left), (top+1, left+1)],
-            #     values = [im1[top, left], im1[top, left+1], im1[top + 1, left], im1[top +1, left+1]],
-            #     fill_value = new_j
-            # )[0]
+    H, W = im1.shape
+    coords = np.array(np.meshgrid(np.arange(H), np.arange(W), indexing='ij'), dtype=np.float64)
+    coords[1] -= d
+    interpolator = interpolate.RegularGridInterpolator((np.arange(H), np.arange(W)), im1, bounds_error=False, fill_value=0)
+    shifted_im1 = interpolator(coords.transpose(1, 2, 0))
 
     return shifted_im1
 
@@ -200,8 +178,7 @@ def negative_log_posterior(d, im0, im1, mu, sigma, alpha):
     d = d.reshape(im0.shape)
     log_post, log_post_grad = stereo_log_posterior(d, im0, im1, mu, sigma, alpha)
 
-    return log_post * -1, log_post_grad.flatten() * -1
-
+    return -log_post, -log_post_grad.flatten()
 
 def stereo(d0, im0, im1, mu, sigma, alpha, method=optim_method()):
     """Estimating the disparity map
@@ -215,10 +192,22 @@ def stereo(d0, im0, im1, mu, sigma, alpha, method=optim_method()):
         d: numpy.float 2d-array estimated value of the disparity
     """
 
-    result = optimize.minimize(negative_log_posterior, d0.flatten(), jac=True, method=method, args=(im0, im1, mu, sigma, alpha))
+    # setting a bound so that disparity value does not go beyond its x position and below 0.
+    bnds = []
+    for i in range(d0.shape[0]):
+        for j in range(d0.shape[1]):
+            bnds.append((0, min(float(j),16))) # maximum value for
+    result = optimize.minimize(negative_log_posterior, d0.flatten(), bounds=bnds, jac=True, method=method,
+                               args=(im0, im1, mu, sigma, alpha), tol= 1e-16, options={'maxiter':60,})
     d = result.x.reshape(im0.shape)
-
+    print(result)
     return d
+
+def downsample(image, new_size):
+
+    resized_img = Image.fromarray(image).resize(new_size)
+    return np.array(resized_img)
+
 
 def coarse2fine(d0, im0, im1, mu, sigma, alpha, num_levels):
     """Coarse-to-fine estimation strategy. Basic idea:
@@ -241,45 +230,31 @@ def coarse2fine(d0, im0, im1, mu, sigma, alpha, num_levels):
 
     from scipy import ndimage
 
-
-    def downsample2(img):
-
-        # convolve image and gauss filter
-        # smooth_img = signal.convolve(img, f, mode='same')
-        smooth_img = ndimage.gaussian_filter(img, sigma = 1.5, mode = 'constant', cval = 0)
-        # take every other row/column
-        downsampled = np.zeros((int(img.shape[0] / 2), int(img.shape[1] / 2)))
-        h, w = downsampled.shape
-
-        for i in range(h):
-            for j in range(w):
-                downsampled[i, j] = smooth_img[i * 2, j * 2]
-
-        return downsampled
-
     # fill the pyramid first
-    pyr_d, pyr_im0, pyr_im1 = [d0], [im0], [im1]
-    d, i0, i1 = d0, im0, im1
+    pyr_d = [d0]
+    pyr_im0 = [im0]
+    pyr_im1 = [im1]
+    d = d0
+    i0 = im0
+    i1 = im1
 
     for i in range(num_levels - 1):
-        # d_f = ndimage.gaussian_filter(d, 1.5, mode='reflect')
-        # i0_f = ndimage.gaussian_filter(i0, 1.5, mode='reflect')
-        # i1_f = ndimage.gaussian_filter(i1, 1.5, mode='reflect')
-
-        d, i0, i1 = downsample2(d), downsample2(i0), downsample2(i1)
+        h, w = d.shape
+        d = downsample(d, (int(d.shape[0]/2), int(d.shape[1]/2)))
+        i0 = downsample(i0, (int(i0.shape[0] / 2), int(i0.shape[1] / 2)))
+        i1 = downsample(i1, (int(i1.shape[0] / 2), int(i1.shape[1] / 2)))
 
         pyr_d.append(d)
         pyr_im0.append(i0)
         pyr_im1.append(i1)
 
-    # do the iteration
+    # do the iteration from lowest resolution
     d = pyr_d[num_levels - 1]
     for i in range(num_levels-1, -1, -1):
         d = stereo(d, pyr_im0[i], pyr_im1[i], mu, sigma, alpha)
         pyr_d[i] = d
-        # upscale the disparity map
-        d = ndimage.zoom(d, 2, order=3)
-        pp = d.shape()
+        # upsample the disparity map and scale by 2, since disparity should be double as image size gets double.
+        d = ndimage.zoom(d, 2, order=3) * 2
 
     return pyr_d
 
@@ -288,8 +263,8 @@ def coarse2fine(d0, im0, im1, mu, sigma, alpha, num_levels):
 # but make sure your final submission can execute this code
 def main():
 
+
     # these are the same functions from Assignment 1
-    # (no graded in this assignment)
     im0, im1, gt = load_data('./data/i0.png', './data/i1.png', './data/gt.png')
     im0, im1 = rgb2gray(im0), rgb2gray(im1)
 
@@ -299,18 +274,63 @@ def main():
     # experiment with other values of alpha
     alpha = 1.0
 
-    # initial disparity map
-    # experiment with constant/random values
-    #d0 = gt
-    d0 = random_disparity(gt.shape)
-    #d0 = constant_disparity(gt.shape, 6)
+    # Initial disparity maps
+    d_gt_init = gt
+    d_const_init = np.full_like(gt, 8)
+    d_rand_init = np.random.uniform(0, 14, gt.shape)
 
-    # Display stereo: Initialized with noise
-    disparity = stereo(d0, im0, im1, mu, sigma, alpha)
+    use_pyramid = False
 
-    # Pyramid
-    num_levels = 3
-    #pyramid = coarse2fine(d0, im0, im1, mu, sigma, alpha, num_levels)
+
+    if not use_pyramid:
+        # Display stereo: Initialized with noise
+        d_gt = stereo(d_gt_init, im0, im1, mu, sigma, alpha)
+        d_const = stereo(d_const_init, im0, im1, mu, sigma, alpha)
+        d_rand = stereo(d_rand_init, im0, im1, mu, sigma, alpha)
+
+    else:
+        # Pyramid
+        num_levels = 3
+        pyramid_gt = coarse2fine(d_gt_init, im0, im1, mu, sigma, alpha, num_levels)
+        pyramid_const = coarse2fine(d_const_init, im0, im1, mu, sigma, alpha, num_levels)
+        pyramid_rand = coarse2fine(d_rand_init, im0, im1, mu, sigma, alpha, num_levels)
+
+    # Final disparities from the finest level of the pyramid
+        d_gt = pyramid_gt[0]
+        d_const = pyramid_const[0]
+        d_rand = pyramid_rand[0]
+
+    # Visualize results
+    fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+
+    axs[0, 0].imshow(gt, cmap='gray')
+    axs[0, 0].set_title('Ground Truth Disparity')
+
+    axs[0, 1].imshow(d_gt, cmap='gray')
+    axs[0, 1].set_title('Estimated Disparity (GT Init)')
+
+    axs[0, 2].imshow(np.abs(gt - d_gt), cmap='gray')
+    axs[0, 2].set_title('Difference (GT)')
+
+    axs[1, 0].imshow(gt, cmap='gray')
+    axs[1, 0].set_title('Ground Truth Disparity')
+
+    axs[1, 1].imshow(d_const, cmap='gray')
+    axs[1, 1].set_title('Estimated Disparity (Const Init)')
+
+    axs[1, 2].imshow(np.abs(gt - d_const), cmap='gray')
+    axs[1, 2].set_title('Difference (Const)')
+
+    axs[2, 0].imshow(gt, cmap='gray')
+    axs[2, 0].set_title('Ground Truth Disparity')
+
+    axs[2, 1].imshow(d_rand, cmap='gray')
+    axs[2, 1].set_title('Estimated Disparity (Rand Init)')
+
+    axs[2, 2].imshow(np.abs(gt - d_rand), cmap='gray')
+    axs[2, 2].set_title('Difference (Rand)')
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
